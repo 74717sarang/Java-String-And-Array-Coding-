5fbcbac04112fb7581f3cab00be8b051f6fb9d11
74717sarang <patilsarang13520@gmail.com>
2024-09-12 12:45:41 +0530

    new

---
+++ Productcrudredux/src/redux/action/productactions.js
@@ -0,0 +1,15 @@
+export const setdata=(prodarr)=>{
+    return{
+        type:"set_data",
+        payload: prodarr
+    }
+
+}
+
+export const adddata=(product)=>{
+    return{
+        type:"add_data",
+        payload: product
+    }
+
+}
