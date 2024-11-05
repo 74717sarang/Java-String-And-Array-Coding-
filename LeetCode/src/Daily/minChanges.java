e8f9e1f69c40a9b55d1c520a72342e20178cdf66
74717sarang <patilsarang13520@gmail.com>
2024-11-05 09:16:47 +0530

    5-11 commit

---
+++ LeetCode/src/Daily/minChanges.java
@@ -0,0 +1,17 @@
+package Daily;
+
+public class minChanges {
+
+	
+	    public int minChangesMethode(String s) {
+	        int ans = 0;
+	        for (int i = 0; i + 1 < s.length(); i += 2) {
+	            if (s.charAt(i) != s.charAt(i + 1)) {
+	                ++ans;
+	            }
+	        }
+	        return ans;
+	    }
+	
+
+}
