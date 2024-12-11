53d8526edab52a6b70861f6adfa4fc3dfc16487b
74717sarang <patilsarang13520@gmail.com>
2024-12-11 08:15:22 +0530

    leetcode 75 challege update

--- LeetCode/src/leetcode75challenge/prefixSum/Leetcode1732.java
+++ LeetCode/src/leetcode75challenge/prefixSum/Leetcode1732.java
@@ -17,8 +17,8 @@ public class Leetcode1732 {
 	
 	public static void main(String[] args) {
 //		Input: gain = [-5,1,5,0,-7]
-//				Output: 1
-//				Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
+//		Output: 1
+//		Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
 	
 	int[]gain = {-5,1,5,0,-7};
 	System.out.println(largestAltitude(gain));
