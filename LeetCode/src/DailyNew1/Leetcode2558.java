ee555b8f57c7ac60c661cdec9c3f6dea5a8e63b3
74717sarang <patilsarang13520@gmail.com>
2024-12-12 23:06:44 +0530

    leetcode daily update

--- LeetCode/src/DailyNew1/Leetcode2558.java
+++ LeetCode/src/DailyNew1/Leetcode2558.java
@@ -1,5 +1,35 @@
 package DailyNew1;
 
+import java.util.PriorityQueue;
+
 public class Leetcode2558 {
 
+	
+	 public static long pickGifts(int[] gifts, int k) {
+	       PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+	        for (int gift : gifts) {
+	            maxHeap.add(gift);
+	        }
+
+	        while (k-- > 0) {
+	            int richestPile = maxHeap.poll();
+	            int remainingGifts = (int) Math.sqrt(richestPile);
+	            maxHeap.add(remainingGifts);
+	        }
+
+	        long totalGifts = 0;
+	        for (int gift : maxHeap) {
+	            totalGifts += gift;
+	        }
+
+	        return totalGifts;
+
+	    }
+	
+	public static void main(String[] args) {
+		
+		int[] gifts = {25,64,9,4,100};
+		int k = 4;
+		System.out.println(pickGifts(gifts, k));
+	}
 }
