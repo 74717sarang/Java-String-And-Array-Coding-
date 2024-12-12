c81355a684e4cde22481f0316a69b7a4be1d1a29
74717sarang <patilsarang13520@gmail.com>
2024-12-12 23:18:39 +0530

    leetcode daily update

--- StringAndArray/src/co/string/Test.java
+++ StringAndArray/src/co/string/Test.java
@@ -1,11 +1,22 @@
 package co.string;
 
+import java.util.Arrays;
+
 public class Test {
 
 	
 	public static void main(String[] args) {
-		int i=64;
-		char ch=64;
-		System.out.println(ch);
+		String str="the new string come";
+		String[]strArr=str.split(" ");
+		
+		for(int i=0;i<strArr.length;i++) {
+			strArr[i]=new StringBuffer(strArr[i]).reverse().toString();
+		}
+		
+		for(int i=0;i<strArr.length;i++) {
+			System.out.print(strArr[i]+" ");
+		}
+		System.out.println();
+		System.out.println(Arrays.toString(strArr));
 	}
 }
