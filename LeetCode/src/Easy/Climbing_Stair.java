032ec2a8d1900ec0f7dded3a8bad1e8cdb3f19a4
74717sarang <patilsarang13520@gmail.com>
2024-12-12 23:02:36 +0530

    leetcode daily update

--- LeetCode/src/Easy/Climbing_Stair.java
+++ LeetCode/src/Easy/Climbing_Stair.java
@@ -7,10 +7,11 @@ public class Climbing_Stair {
 	 public int climbStairs(int n) {
 	        int []res=new int[n+1];
 	        if(n==1) return 1;
-	        res[1]=1;
-	        res[2]=2;
+	        res[1]=1;     //  for one step only one way to go is 1
+	        res[2]=2;     //  for two step  one and agin one
+	                       // and  to go  direct 2
 	        for(int i=3;i<=n;i++){
-	            res[i]=res[i-1]+res[i-2];
+	            res[i]=res[i-1]+res[i-2];   // for i is= i-1 + i-2
 	        }
 	        return res[n];
 	    }
@@ -21,7 +22,7 @@ public class Climbing_Stair {
 		 Climbing_Stair obj=new Climbing_Stair();
 		 
 		 
-		 System.out.println(obj.climbStairs(3));  //34
+		 System.out.println(obj.climbStairs(8));  //34
 	}
 	
 	
