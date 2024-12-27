196b416007115ead7a01510b9f1b6aeac3a20494
74717sarang <patilsarang13520@gmail.com>
2024-12-27 20:28:35 +0530

    daily update

---
+++ LeetCode/src/DailyNew1/Leetcode1014.java
@@ -0,0 +1,25 @@
+package DailyNew1;
+
+public class Leetcode1014 {
+
+	public static int maxScoreSightseeingPair(int[] values) {
+		int maxScore = 0;
+		int maxI = values[0]; // Keeps track of the best value of (values[i] + i) so far
+
+		for (int j = 1; j < values.length; j++) {
+			maxScore = Math.max(maxScore, maxI + values[j] - j);
+			maxI = Math.max(maxI, values[j] + j); // Update maxI for the next iteration
+		}
+
+		return maxScore;
+	}
+
+	public static void main(String[] args) {
+
+		int[] values = {8,1,5,2,6};
+	       //output 11	
+		System.out.println(maxScoreSightseeingPair(values));
+		
+		
+	}
+}
