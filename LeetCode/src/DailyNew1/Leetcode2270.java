42945339d67b2614c6cbd2311d02e247d2362e9b
74717sarang <patilsarang13520@gmail.com>
2025-01-03 13:58:32 +0530

    leetcode update

--- LeetCode/src/DailyNew1/Leetcode2270.java
+++ LeetCode/src/DailyNew1/Leetcode2270.java
@@ -1,5 +1,37 @@
 package DailyNew1;
 
+import Daily.minChanges;
+
 public class Leetcode2270 {
 
+	
+	    public static int waysToSplitArray(int[] nums) {
+	        int n = nums.length;
+	        long totalSum = 0;
+	        for (int num : nums) {
+	            totalSum += num;
+	        }
+	        
+	        long leftSum = 0;
+	        int ways = 0;
+	        for (int i = 0; i < n - 1; i++) {
+	            leftSum += nums[i];
+	            if (leftSum >= totalSum - leftSum) {
+	                ways++;
+	            }
+	        }
+	        
+	        return ways;
+	    }
+
+	public static void main(String[] args) {
+		
+		
+//		Input: nums = [10,4,-8,7]
+//				Output: 2
+	
+	int[]nums = {10,4,-8,7};
+	System.out.println(waysToSplitArray(nums));
+	
+	}
 }
