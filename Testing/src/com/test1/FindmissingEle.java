772bef63975ebdb8f37c6782e0c6030bfd2c47b4
74717sarang <patilsarang13520@gmail.com>
2025-01-21 19:33:22 +0530

    update in testing add same testclass for practics

---
+++ Testing/src/com/test1/FindmissingEle.java
@@ -0,0 +1,27 @@
+package com.test1;
+
+public class FindmissingEle {
+
+	private static int findEle(int[] arr, int n) {
+
+		// other way is sort arr and return n-1 ele
+
+		// this using formule for sum of N natural numbers
+		int totalSum = n * (n + 1) / 2;
+		int actualSum = 0;
+		for (int i : arr) {
+			actualSum += i;
+		}
+
+		return totalSum - actualSum;
+
+	}
+
+	public static void main(String[] args) {
+
+		int[] arr = { 1, 2, 4, 5, 6 };
+		int n = 6;
+		System.out.println("Missing Element:" + findEle(arr, n));
+	}
+
+}
---
+++ Testing/src/com/test1/SecondLargestEle.java
@@ -0,0 +1,44 @@
+package com.test1;
+
+public class SecondLargestEle {
+
+	
+	private static int findSecondLargest(int[] arr) {
+
+//		shortcut way in two step
+//		Arrays.sort(arr);
+//		return arr[arr.length-2];
+		
+		int largest=0;
+		int secondlargest=0;
+		
+//		always start with both is zero
+		// then when problem is ouccer
+//          1.when all ele is -ve
+//		    2. when all ele is 0. in both case probleam occure
+		
+//		then you tell that now take Integer.MIN_VALUE for both to handle bag
+//		int largest = Integer.MIN_VALUE, secondLargest = Integer.MIN_VALUE;
+		
+		for (int num : arr) {
+			if(num>largest) {
+				secondlargest=largest;
+				largest=num;
+			}
+			else if(num >secondlargest && num < largest) {
+				secondlargest=num;
+			}
+		}
+		return secondlargest;
+	}
+	
+	
+	public static void main(String[] args) {
+
+		  int[] arr = {10, -20, 4, -95, 99, 45};
+	        System.out.println("Second largest element: " + findSecondLargest(arr));
+	}
+
+	
+
+}
