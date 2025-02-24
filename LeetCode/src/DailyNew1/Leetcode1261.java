55350df839afaa184005df0752e0ba48076c33f4
74717sarang <patilsarang13520@gmail.com>
2025-02-24 11:39:03 +0530

    update in leetcode daily

--- LeetCode/src/DailyNew1/Leetcode1261.java
+++ LeetCode/src/DailyNew1/Leetcode1261.java
@@ -1,10 +1,38 @@
 package DailyNew1;
 
+import java.util.HashSet;
+
+import DailyNew1.Leetcode2415.TreeNode;
+
 public class Leetcode1261 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	class FindElements {
+	    HashSet<Integer> set; // Stores all recovered values
 
+	    public FindElements(TreeNode root) {
+	        set = new HashSet<>();
+	        recoverTree(root, 0);
+	    }
+	    
+	    public boolean find(int target) {
+	        return set.contains(target);
+	    }
+
+	    private void recoverTree(TreeNode root, int value) {
+	        if (root == null) return;
+	        set.add(value);
+	        root.val = value;
+	        recoverTree(root.left, 2 * value + 1);
+	        recoverTree(root.right, 2 * value + 2);
+	    }
+	}
+	
+	
+	
+	public static void main(String[] args) {
+
+		
+		
 	}
 
 }
---
+++ Testing/src/com/test2/BraketCheck.java
@@ -0,0 +1,58 @@
+package com.test2;
+
+import java.util.Stack;
+
+public class BraketCheck {
+
+	private static boolean isBalanced(String str) {
+		Stack<Character> stack = new Stack<>();
+		
+		for (char c : str.toCharArray()) {
+			if (c == '[' || c == '{' || c == '(') {
+				stack.push(c);
+			} else {
+				if (stack.isEmpty())
+					return false;
+				char top = stack.pop();
+				if ((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '[')) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	
+	private static boolean isBalancedcheck(String str) {
+		Stack<Character> stack = new Stack<>();
+
+		for(char c:str.toCharArray()) {
+			if(c=='(') {
+				stack.push(c);
+			}
+			else {
+				if(stack.isEmpty()) return false;
+				char top=stack.pop();
+				if(c==')'&& top!='(') {
+					return false;
+				}
+			}
+		}
+		
+		
+		
+		return true;
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println(isBalanced("{[()]}")); // true
+		System.out.println(isBalanced("{[(])}")); // false
+		
+		System.out.println(isBalanced("{{{}}}")); // true
+
+	}
+
+}
